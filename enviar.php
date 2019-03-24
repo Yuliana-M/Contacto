5ceab9fb789127bbc1e4de9c10111d5f80778eb1
Yuliana-Munoz <48842194+Yuliana-Munoz@users.noreply.github.com>
2019-03-23 23:28:11 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+$destino= 'yulianatobar24@gmail.com';
+$nombre= $_POST['nome'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+$contenido = 'nome: ' . $nome. '\nEmail: ' . $email . '\nSubject: ' $subject . '\nMessage: $message ';
+mail($destino, $subject, 'contacto', $contenido);
+header('location:Gracias.html');
+
+?>
