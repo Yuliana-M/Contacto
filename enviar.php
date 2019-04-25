e7545b9368a090feacf53746538945b9f9764440
Yuliana-Munoz <48842194+Yuliana-Munoz@users.noreply.github.com>
2019-04-25 15:28:42 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,11 +1,26 @@
 <?php
-$destino= 'yulianatobar24@gmail.com';
-$nombre= $_POST['nome'];
-$email= $_POST['email'];
-$subject= $_POST['subject'];
-$message= $_POST['message'];
-$contenido = 'nome: ' . $nome. '\nEmail: ' . $email . '\nSubject: ' $subject . '\nMessage: $message ';
-mail($destino, $subject, 'contacto', $contenido);
-header('location:Gracias.html');
-
+    $to = 'yulianatobar24@gmail.com';
+    $firstname = $_POST["fname"];
+    $email= $_POST["email"];
+    $text= $_POST["message"];
+    $phone= $_POST["phone"];
+    
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $message ='<table style="width:100%">
+        <tr>
+            <td>'.$firstname.'  '.$laststname.'</td>
+        </tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>phone: '.$phone.'</td></tr>
+        <tr><td>Text: '.$text.'</td></tr>
+        
+    </table>';
+    if (@mail($to, $email, $message, $headers))
+    {
+        echo 'The message has been sent.';
+    }else{
+        echo 'failed';
+    }
 ?>
